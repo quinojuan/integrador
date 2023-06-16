0173a43a3fa7b434272049aafd5e051dfc2c3537
Juan Quino <quinojuan@gmail.com>
2023-06-16 20:10:45 -0300

    boton agregar producto ok

--- script.js
+++ script.js
@@ -1,48 +1,69 @@
 // Diccionario
 
-let diccionario = [
-  {
-    palabra: "peregrino",
-    definicion:
-      "Persona Que anda o viaja por tierras extrañas que presentan dificultades.",
-  },
-  {
-    palabra: "berenjena",
-    definicion:
-      "planta de fruto comestible, generalmente anual, del género Solanum dentro de la familia de las solanáceas",
-  },
-  {
-    palabra: "bucle",
-    definicion:
-      "secuencia de instrucciones de código que se ejecuta repetidas veces.//Curva en forma de rizo que se forma en determinadas cosas. ",
-  },
-  {
-    palabra: "estetoscopio",
-    definicion:
-      "Instrumento médico que sirve para explorar los sonidos producidos por los órganos de las cavidades del pecho y del abdomen.",
-  },
-  {
-    palabra: "recoleccion",
-    definicion:
-      "Acción o actividad de recolectar los frutos de la tierra. //Conjunto de frutos recolectados.",
-  },
-];
+// let diccionario = [
+//   {
+//     palabra: "peregrino",
+//     definicion:
+//       "Persona Que anda o viaja por tierras extrañas que presentan dificultades.",
+//   },
+//   {
+//     palabra: "berenjena",
+//     definicion:
+//       "planta de fruto comestible, generalmente anual, del género Solanum dentro de la familia de las solanáceas",
+//   },
+//   {
+//     palabra: "bucle",
+//     definicion:
+//       "secuencia de instrucciones de código que se ejecuta repetidas veces.//Curva en forma de rizo que se forma en determinadas cosas. ",
+//   },
+//   {
+//     palabra: "estetoscopio",
+//     definicion:
+//       "Instrumento médico que sirve para explorar los sonidos producidos por los órganos de las cavidades del pecho y del abdomen.",
+//   },
+//   {
+//     palabra: "recoleccion",
+//     definicion:
+//       "Acción o actividad de recolectar los frutos de la tierra. //Conjunto de frutos recolectados.",
+//   },
+// ];
 
-var input = document.getElementById("palabra");
-var button = document.getElementById("definicion");
-var recuadro = document.getElementById("recuadroCeleste");
+// var input = document.getElementById("palabra");
+// var button = document.getElementById("definicion");
+// var recuadro = document.getElementById("recuadroCeleste");
 
-function buscarPalabra(palabra) {
-  for (let i = 0; i < diccionario.length; i++) {
-    if (diccionario[i].palabra === palabra) {
-      return (recuadro.innerHTML = diccionario[i].definicion);
-    }
-  }
-  return (recuadro.innerHTML =
-    "lo sentimos, nuestro diccionario no dispone de esta esta definición por el momento");
-}
+// function buscarPalabra(palabra) {
+//   for (let i = 0; i < diccionario.length; i++) {
+//     if (diccionario[i].palabra === palabra) {
+//       return (recuadro.innerHTML = diccionario[i].definicion);
+//     }
+//   }
+//   return (recuadro.innerHTML =
+//     "lo sentimos, nuestro diccionario no dispone de esta esta definición por el momento");
+// }
 
-button.addEventListener("click", () => {
-  var valorInput = input.value;
-  buscarPalabra(valorInput);
+// button.addEventListener("click", () => {
+//   var valorInput = input.value;
+//   buscarPalabra(valorInput);
+// });
+
+// Comprando muebles
+
+var botonComprar = document.getElementById("comprar");
+var botonSacarProducto = document.getElementById("sacarItem")
+var circulo = document.getElementById("circulo");
+var valorCirculo = 0;
+circulo.innerHTML = valorCirculo
+
+botonComprar.addEventListener("click", () => {
+  valorCirculo++;
+
+  circulo.innerHTML = valorCirculo;
 });
+
+botonSacarProducto.addEventListener("click", () => {
+    if(valorCirculo > 0) { // con este if evito que se me generen numeros negativos en la cantidad
+        valorCirculo--;
+        circulo.innerHTML = valorCirculo
+    }
+})
